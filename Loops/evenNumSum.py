d767b089d9b32399c98234948203b79106b2bb54
KADIRS-PC\Kadir BALIKCI <kadir.balikci@mobimeo.com>
2023-09-17 17:04:06 +0200

    day 5-1

---
+++ Loops/evenNumSum.py
@@ -0,0 +1,6 @@
+sum=0
+for i in range(2,101,2):
+    print(i)
+    sum+=i
+
+print(sum)
---
+++ Loops/fizzBuzz.py
@@ -0,0 +1,8 @@
+for i in range(1, 101):
+    if i%3==0 and i%5==0:
+        i="fizzbuzz"
+    elif i%3==0 and i%5!=0:
+        i="fizz"
+    elif i%5==0 and i%3!=0:
+        i="buzz"
+    print(i)
