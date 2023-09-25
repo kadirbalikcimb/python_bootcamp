acbdd90470459eed1836ada7286fb359557305a9
kadirbalikcimb <kadir.balikci@mobimeo.com>
2023-09-25 13:29:30 +0200

    cydeo codes

---
+++ StringFuns/slice.py
@@ -0,0 +1,19 @@
+s = 'Java Programming Language'
+
+s2 = s[5:16]
+
+print(s2)
+
+print(s[:4])
+
+s3 = s[::-1]
+
+print(s3)
+
+s4 = "Python Programming"
+
+#s5 = str(reversed(s4))
+
+print(s4)
+
+print(s4[7:])
